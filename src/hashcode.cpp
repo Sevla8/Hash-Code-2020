e7956f40fd1f1ee829e9c541f066eeecd54ea136
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2020-01-26 11:57:50 -0500

    version 2 O(n)

--- src/hashcode.cpp
+++ src/hashcode.cpp
@@ -5,7 +5,26 @@ Hashcode::Hashcode() {}
 Hashcode::~Hashcode() {}
 
 void Hashcode::calculate() {
+	this->nbSelectedPizza = 0;
+	this->nbSlice = 0;
 
+	unsigned int it = this->nbPizza-1;
+	while (this->slices[it] > this->nbSliceMax) --it;
+	this->nbSlice += this->slices[it];
+	this->selected[it] = true;
+
+	for (int jt = it-1; jt >= 0; --jt) {
+		if (this->nbSlice + this->slices[jt] < this->nbSliceMax) {
+			this->nbSlice += this->slices[jt];
+			this->selected[jt] = true;
+		}
+	}
+
+	for (unsigned int i = 0; i <this->nbPizza; i += 1) {
+		if (this->selected[i]) {
+			this->nbSelectedPizza += 1;
+		}
+	}
 }
 
 std::istream& operator>>(std::istream& is, Hashcode& ha) {
@@ -14,6 +33,7 @@ std::istream& operator>>(std::istream& is, Hashcode& ha) {
 
 	ha.slices = new unsigned int[ha.nbPizza];
 	ha.selected = new bool[ha.nbPizza];
+	for (unsigned int i = 0; i < ha.nbPizza; i += 1) ha.selected[i] = false;
 
 	for (unsigned int it = 0; it < ha.nbPizza; ++it) {
 		is >> ha.slices[it];
