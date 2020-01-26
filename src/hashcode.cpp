9e0f88f98630e04d738bd11a6429bf1bf9c5233a
Brandon ALVES BARBOSA <brandon.alves.barbosa@gmail.com>
2020-01-26 12:09:10 -0500

    r2w super

--- src/hashcode.cpp
+++ src/hashcode.cpp
@@ -8,17 +8,17 @@ void Hashcode::calculate() {
 	this->nbSelectedPizza = 0;
 	this->nbSlice = 0;
 
-	unsigned int it = this->nbPizza-1;
-	while (this->slices[it] > this->nbSliceMax) --it;
-	this->nbSlice += this->slices[it];
-	this->selected[it] = true;
+	// unsigned int it = this->nbPizza-1;
+	// while (this->slices[it] > this->nbSliceMax) --it;
+	// this->nbSlice += this->slices[it];
+	// this->selected[it] = true;
 
-	for (int jt = it-1; jt >= 0; --jt) {
-		if (this->nbSlice + this->slices[jt] < this->nbSliceMax) {
-			this->nbSlice += this->slices[jt];
-			this->selected[jt] = true;
-		}
-	}
+	// for (int jt = it-1; jt >= 0; --jt) {
+	// 	if (this->nbSlice + this->slices[jt] < this->nbSliceMax) {
+	// 		this->nbSlice += this->slices[jt];
+	// 		this->selected[jt] = true;
+	// 	}
+	// }
 
 	for (unsigned int i = 0; i <this->nbPizza; i += 1) {
 		if (this->selected[i]) {
